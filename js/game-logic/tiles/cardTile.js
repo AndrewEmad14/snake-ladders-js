4e51ab66acb99fb033c2f29f8b280baaf72ec52f
ZeyadHMostafa <ZeyadHMostafa@gmail.com>
2026-01-24 15:04:12 +0200

    fixed card basic card behavior

--- js/game-logic/tiles/cardTile.js
+++ js/game-logic/tiles/cardTile.js
@@ -6,34 +6,34 @@ import Tile from "../tiles.js";
  * @augments Tile
  */
 export default class CardTile extends Tile {
-  /**
-   * @param {Point} start which the parent already has we will call his constructor
-   * 
-   */
-  constructor(start) {
-    //TODO: validate
-    super(start);
-  }
+	/**
+	 * @param {Point} start which the parent already has we will call his constructor
+	 *
+	 */
+	constructor(start) {
+		//TODO: validate
+		super(start);
+	}
 
-  /**
-   * @override
-   */
-  get name(){
-    return "card";
-  }
+	/**
+	 * @override
+	 */
+	get name(){
+		return "card";
+	}
 
-  /**
-   * add a power-up card to the player 
-   * @inheritdoc
-   * @override
-   * @param {PlayerGameData} player the player who stepped on it
-   */
-  effect(game,player) {
-    // Note: this relies on Points being immutable
-    // otherwise we should copy data firs
-    // TODO: confirm if this is fine
-    console.log("i have been pushed");
-    let card = getCard();
-    player.pushCard(card)
-  }
+	/**
+	 * add a power-up card to the player
+	 * @inheritdoc
+	 * @override
+	 * @param {PlayerGameData} player the player who stepped on it
+	 */
+	effect(game,player) {
+		// Note: this relies on Points being immutable
+		// otherwise we should copy data firs
+		// TODO: confirm if this is fine
+		console.log("i have been pushed");
+		let card = getCard();
+		player.pushCard(card);
+	}
 }
--- js/utils/fetchRandomCard.js
+++ js/utils/fetchRandomCard.js
@@ -8,38 +8,38 @@ import { diceRoll } from "./utils.js";
 
 
 export function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 let numberOfCardTypes = 1;
 
 export function getCard() {
-  let randomCard = diceRoll(numberOfCardTypes);
-  switch (randomCard) {
-    case 1:
-      return new jumpCard(5);
+	let randomCard = diceRoll(numberOfCardTypes);
+	switch (randomCard) {
+	case 1:
+		return new jumpCard(5);
 
-    case 2:
-      //
-      return;
-    case 3:
-      //
-      return;
-    case 4:
-      //
-      return;
-    case 5:
-      //
-      return;
-    case 6:
-      //
-      return;
-    case 7:
-      //
-      return;
-    case 8:
-      return;
-    case 9:
-      return;
-  }
+	case 2:
+		//
+		return;
+	case 3:
+		//
+		return;
+	case 4:
+		//
+		return;
+	case 5:
+		//
+		return;
+	case 6:
+		//
+		return;
+	case 7:
+		//
+		return;
+	case 8:
+		return;
+	case 9:
+		return;
+	}
 }
